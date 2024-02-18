25ab86fa6dcfb1f15ab8484cc328549b1a251b66
Kizzull <github@kizzull.com>
2024-02-18 20:16:05 +0000

    CORS support for previewThumbnails

--- src/js/plyr.d.ts
+++ src/js/plyr.d.ts
@@ -584,6 +584,7 @@ declare namespace Plyr {
   interface PreviewThumbnailsOptions {
     enabled?: boolean;
     src?: string | string[];
+    withCredentials?: boolean;
   }
 
   interface MediaMetadataArtwork {
